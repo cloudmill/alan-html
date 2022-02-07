9e06a685b750c5f5f4df77cfbccb92355f37da2d
szatolokin <staszatolokin28@gmail.com>
2022-02-07 17:46:02 +0300

    Update

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("from github pages");
+window.addEventListener("DOMContentLoaded", () => {
+  try {
+    const preloaderVid = document.getElementById("preloader-vid");
+    preloaderVid.playbackRate = 1.5;
+  } catch {}
+
+  try {
+    const allVideo = document.getElementsByTagName("video");
+    for (video of allVideo) {
+      video.setAttribute("playsinline", "");
+      video.setAttribute("muted", "");
+      video.play();
+    }
+  } catch {}
+});
