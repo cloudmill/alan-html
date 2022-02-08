78a8aece8634e76e77916e25ea9c110d416976f1
szatolokin <staszatolokin28@gmail.com>
2022-02-08 12:39:38 +0300

    Update

--- script.js
+++ script.js
@@ -117,4 +117,155 @@ window.addEventListener("DOMContentLoaded", () => {
       );
     }
   }
+
+  // waves
+  {
+    var waves1 = new SineWaves({
+      el: document.getElementById("waves"),
+
+      speed: 7.5,
+      running: false,
+
+      width: function () {
+        return $(window).width();
+      },
+
+      height: function () {
+        return $(window).height();
+      },
+
+      ease: "SineInOut",
+
+      wavesWidth: "100%",
+
+      waves: [
+        {
+          timeModifier: 3,
+          lineWidth: 1,
+          amplitude: -400,
+          waveLength: 400,
+        },
+        {
+          type: "SineWave",
+          segmentLength: 1,
+        },
+        {
+          timeModifier: 2,
+          lineWidth: 1,
+          amplitude: -370,
+          wavelength: 100,
+        },
+        {
+          timeModifier: 1,
+          lineWidth: 1,
+          amplitude: -500,
+          wavelength: 350,
+        },
+        {
+          timeModifier: 0.5,
+          lineWidth: 1,
+          amplitude: -380,
+          wavelength: 180,
+        },
+        {
+          timeModifier: 0.4,
+          lineWidth: 1,
+          amplitude: -300,
+          wavelength: 200,
+        },
+      ],
+
+      // Called on window resize
+      resizeEvent: function () {
+        var gradient = this.ctx.createLinearGradient(0, 0, this.width, 0);
+        gradient.addColorStop(0.3, "rgba(48, 166, 255, 1)");
+        gradient.addColorStop(0.5, "rgba(22, 106, 240, 1)");
+        gradient.addColorStop(0.7, "rgba(22, 0, 157, 1)");
+
+        var index = -1;
+        var length = this.waves.length;
+        for (index = 0; index < length; index++) {
+          this.waves[index].strokeStyle = gradient;
+        }
+
+        // Clean Up
+      },
+    });
+    var waves2 = new SineWaves({
+      el: document.getElementById("waves_2"),
+
+      speed: 7.5,
+      running: false,
+
+      width: function () {
+        return $(window).width();
+      },
+
+      height: function () {
+        return $(window).height();
+      },
+
+      ease: "SineInOut",
+
+      wavesWidth: "100%",
+
+      waves: [
+        {
+          timeModifier: 3,
+          lineWidth: 1,
+          amplitude: -400,
+          waveLength: 400,
+        },
+        {
+          type: "SineWave",
+          segmentLength: 1,
+        },
+        {
+          timeModifier: 2,
+          lineWidth: 1,
+          amplitude: -370,
+          wavelength: 100,
+        },
+        {
+          timeModifier: 1,
+          lineWidth: 1,
+          amplitude: -500,
+          wavelength: 350,
+        },
+        {
+          timeModifier: 0.5,
+          lineWidth: 1,
+          amplitude: -380,
+          wavelength: 180,
+        },
+        {
+          timeModifier: 0.4,
+          lineWidth: 1,
+          amplitude: -300,
+          wavelength: 200,
+        },
+      ],
+
+      // Called on window resize
+      resizeEvent: function () {
+        var gradient = this.ctx.createLinearGradient(0, 0, this.width, 0);
+        gradient.addColorStop(0.3, "rgba(48, 166, 255, 1)");
+        gradient.addColorStop(0.5, "rgba(22, 106, 240, 1)");
+        gradient.addColorStop(0.7, "rgba(22, 0, 157, 1)");
+
+        var index = -1;
+        var length = this.waves.length;
+        for (index = 0; index < length; index++) {
+          this.waves[index].strokeStyle = gradient;
+        }
+
+        // Clean Up
+      },
+    });
+
+    setInterval(() => {
+      waves1.update();
+      waves2.update();
+    }, 1000 / 24);
+  }
 });
