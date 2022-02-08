985a286f58c8b7d41ce8f33d45df416d921a69ba
szatolokin <staszatolokin28@gmail.com>
2022-02-08 09:56:18 +0300

    Update

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
+  // legacy
   try {
     const preloaderVid = document.getElementById("preloader-vid");
     preloaderVid.playbackRate = 1.5;
@@ -13,13 +14,33 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   } catch {}
 
+  // video play/pause on scroll
   const allVideo = document.querySelectorAll("video:not(.always-play)");
 
   const videoObserver = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      console.log(entry);
+    entries.forEach(({ isIntersecting, target }) => {
+      try {
+        if (isIntersecting) {
+          target.play();
+        } else {
+          target.pause();
+        }
+      } catch {}
     });
   });
 
+  const updateVideoObserver = () => {
+    videoObserver.rootMargin = `${document.documentElement.clientHeight}px 0`;
+  };
+  updateVideoObserver();
+
+  [("load", "resize")].forEach((eventType) =>
+    window.addEventListener(eventType, updateVideoObserver)
+  );
+
   allVideo.forEach((video) => videoObserver.observe(video));
+
+  setInterval(() => {
+    allVideo.forEach((video) => console.log(video.currentTime));
+  }, 1000);
 });
