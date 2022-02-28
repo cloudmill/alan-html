4afaacb27d4a26570e68448362ea65a87c72aa7f
szatolokin <staszatolokin28@gmail.com>
2022-02-28 17:38:35 +0300

    Update

--- script.js
+++ script.js
@@ -337,47 +337,79 @@ window.addEventListener("DOMContentLoaded", () => {
 
   // alan-btn
   {
+    const ANIMATION_INTERVAL = 3000;
+    const ANIMATION_STEPS = [
+      "listening",
+      "intermediate",
+      "understood",
+      "speaking",
+    ];
+
     const allAlanBtn = document.querySelectorAll(".alan-btn");
 
-    const wait = (time) => new Promise((res) => setTimeout(() => res(), time));
-
-    const startBtnAnimation = (btn) => {
-      wait(0)
-        .then(() => {
-          btn.switchState("listening");
-        })
-        .then(() => {
-          return wait(3000).then(() => {
-            btn.switchState("intermediate");
-          });
-        })
-        .then(() => {
-          return wait(3000).then(() => {
-            btn.switchState("understood");
-          });
-        })
-        .then(() => {
-          return wait(3000).then(() => {
-            btn.switchState("speaking");
-          });
-        })
-        .then(() => {
-          startBtnAnimation(btn);
-        });
-    };
-
     allAlanBtn.forEach((alanBtn) => {
-      console.log(alanBtn);
+      const state = {
+        el: alanBtn,
+        wrapper: alanBtn.closest(".alan-btn-wrapper"),
 
-      const btn = alanBtnFake({
-        rootEl: alanBtn,
-        position: "absolute",
-        top: "0",
-        left: "0",
-        size: "50",
-      });
+        size: null,
+        btn: null,
+        step: null,
+        interval: null,
+      };
 
-      startBtnAnimation(btn);
+      const updateSize = () => {
+        state.size = String(
+          Math.ceil(state.wrapper.getBoundingClientRect().width)
+        );
+      };
+      const updateBtn = () => {
+        state.btn = alanBtnFake({
+          rootEl: state.el,
+          position: "absolute",
+          top: "0",
+          left: "0",
+          size: state.size,
+        });
+      };
+      const updateStep = () => {
+        state.step = 0;
+      };
+
+      const normalizeStep = () => {
+        const stepsCount = ANIMATION_STEPS.length;
+
+        if (state.step >= stepsCount) {
+          state.step %= stepsCount;
+        }
+      };
+      const animateBtn = () => {
+        normalizeStep();
+
+        state.btn.switchState(ANIMATION_STEPS[state.step++]);
+
+        console.log("animateBtn", state);
+      };
+      const updateInterval = () => {
+        if (state.interval) {
+          clearInterval(state.interval);
+        }
+
+        state.interval = setInterval(animateBtn, ANIMATION_INTERVAL);
+      };
+
+      const update = () => {
+        updateSize();
+        updateBtn();
+        updateStep();
+        updateInterval();
+
+        console.log("update", state);
+      };
+
+      update();
+      window.addEventListener("load", update);
+      window.addEventListener("resize", update);
     });
   }
 });
